2604ac0ffda4f7f250ae9ac2104daa690dded650
JamesB-Wood <james.brian.wood@gmail.com>
2024-04-27 21:33:08 +1000

    Move Eye tracking

--- dashboard/dashboard/components/eye_tracking/__init__.py
+++ dashboard/dashboard/components/eye_tracking/__init__.py
@@ -1,8 +1,10 @@
 import cv2
 from .gaze_tracking import GazeTracking
-from queue import Queue
+from queue import LifoQueue
+from collections import deque
 
-def start_tracking_thread(queue: Queue):
+
+def start_tracking_thread(queue: deque):
     gaze = GazeTracking()
     webcam = cv2.VideoCapture(0)
 
@@ -14,23 +16,17 @@ def start_tracking_thread(queue: Queue):
         gaze.refresh(frame)
 
         frame = gaze.annotated_frame()
-        text = ""
+        text = "Looking away"
 
-        if gaze.is_blinking():
-            text = "Blinking"
-        elif gaze.is_right():
-            text = "Looking right"
-        elif gaze.is_left():
-            text = "Looking left"
-        elif gaze.is_center():
-            text = "Looking center"
+        if gaze.is_blinking() or gaze.is_right() or gaze.is_left() or gaze.is_center():
+            text = "Looking at Screen"
+
+        queue.append(text)
 
         cv2.putText(frame, text, (90, 60), cv2.FONT_HERSHEY_DUPLEX, 1.6, (147, 58, 31), 2)
 
         left_pupil = gaze.pupil_left_coords()
         right_pupil = gaze.pupil_right_coords()
-        cv2.putText(frame, "Left pupil:  " + str(left_pupil), (90, 130), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
-        cv2.putText(frame, "Right pupil: " + str(right_pupil), (90, 165), cv2.FONT_HERSHEY_DUPLEX, 0.9, (147, 58, 31), 1)
 
         cv2.imshow("Demo", frame)
 
