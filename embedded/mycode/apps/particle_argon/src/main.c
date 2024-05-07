3c305f84e68e836380fd6bab64789f0abb440eb9
Yuang Li <s4682317@student.uq.edu.au>
2024-05-07 20:19:11 +1000

    Fixed broadcaster in base, Implemented bluetooth scanning for particle argon

--- embedded/mycode/apps/particle_argon/src/main.c
+++ embedded/mycode/apps/particle_argon/src/main.c
@@ -4,14 +4,60 @@
 #include <zephyr/usb/usbd.h>
 #include <zephyr/drivers/uart.h>
 
+#include <zephyr/types.h>
+#include <stddef.h>
+#include <zephyr/sys/util.h>
+
+#include <zephyr/bluetooth/bluetooth.h>
+#include <zephyr/bluetooth/hci.h>
+
+#define NODE_BASE "F6:F2:9E:CA:3A:DE"
+
+static struct bt_conn *default_conn;
+
+static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
+			 struct net_buf_simple *ad)
+{
+	char addr_str[BT_ADDR_LE_STR_LEN];
+
+	if (default_conn) {
+		return;
+	}
+
+	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
+
+	if (strncmp(addr_str, NODE_BASE, strlen(NODE_BASE)) == 0) {
+		// Majors: ad->data[25], ad->data[26]
+		// Minors: ad->data[27], ad->data[28]
+	}
+}
+
 int main(void)
 {
 	if (usb_enable(NULL)) {
 		return 0;
 	}
 
-	while (1) {
-		printk("Hello World! %s\n", CONFIG_ARCH);
-		k_sleep(K_SECONDS(1));
+	struct bt_le_scan_param scan_param = {
+		.type       = BT_HCI_LE_SCAN_PASSIVE,
+		.options    = BT_LE_SCAN_OPT_NONE,
+		.interval   = 0x0010,
+		.window     = 0x0010,
+	};
+	int err;
+
+	/* Initialize the Bluetooth Subsystem */
+	err = bt_enable(NULL);
+	if (err) {
+		printk("Bluetooth init failed (err %d)\n", err);
+		return 0;
+	}
+
+	printk("Bluetooth initialized\n");
+
+	err = bt_le_scan_start(&scan_param, device_found);
+	if (err) {
+		printk("Starting scanning failed (err %d)\n", err);
+		return 0;
 	}
 }
--- embedded/mycode/mylib/scan_adv.c
+++ embedded/mycode/mylib/scan_adv.c
@@ -28,6 +28,28 @@ const struct device *const dev = DEVICE_DT_GET_ONE(panasonic_amg88xx);
 
 int ret;
 
+/*
+ * Set iBeacon advertisement data.
+ *
+ * UUID:  18ee1516-016b-4bec-ad96-bcb96d166e97
+ * Major: 0
+ * Minor: 0
+ * RSSI:  -56 dBm
+ */
+struct bt_data ad[] = {
+    BT_DATA_BYTES(BT_DATA_FLAGS, BT_LE_AD_NO_BREDR),
+    BT_DATA_BYTES(BT_DATA_MANUFACTURER_DATA, 0x4c, 0x00, /* Apple */
+                  0x02, 0x15,                            /* iBeacon */
+                  0x18, 0xee, 0x15, 0x16,                /* UUID[15..12] */
+                  0x01, 0x6b,                            /* UUID[11..10] */
+                  0x4b, 0xec,                            /* UUID[9..8] */
+                  0xad, 0x96,                            /* UUID[7..6] */
+                  0xbc, 0xb9, 0x6d, 0x16, 0x6e, 0x97,    /* UUID[5..0] */
+                  0x00, 0x00,                            /* Major */
+                  0x00, 0x00,                            /* Minor */
+                  IBEACON_RSSI) /* Calibrated RSSI @ 1m */
+};
+
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			 struct net_buf_simple *ad_rev)
 {
@@ -100,6 +122,14 @@ int scan_adv_thread(void)
 	err = bt_le_scan_start(&scan_param, device_found);
 	if (err) {
 		printk("Starting scanning failed (err %d)\n", err);
+		return 0;
+	}
+
+	/* Start advertising */
+	err = bt_le_adv_start(BT_LE_ADV_NCONN_IDENTITY, ad, ARRAY_SIZE(ad),
+					NULL, 0);
+	if (err) {
+		printk("Advertising failed to start (err %d)\n", err);
 		return 0;
 	}
 
