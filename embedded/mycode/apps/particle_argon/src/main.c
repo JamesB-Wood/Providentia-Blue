abdee6daf6c076d6f948aabb704d88cb4efda89b
Yuang Li <s4682317@student.uq.edu.au>
2024-05-09 15:52:22 +1000

    Made Progress on I2C to communicate with Crikit

--- embedded/mycode/apps/particle_argon/src/main.c
+++ embedded/mycode/apps/particle_argon/src/main.c
@@ -15,9 +15,18 @@
 
 #define NODE_BASE "F6:F2:9E:CA:3A:DE"
 
+#define CRICKIT_SERVO 17
+#define CRICKIT_DUTY_CYCLE_MAX 65535
+#define CRICKIT_ADDR 0x49
+#define CRICKIT_TIMER_FREQ 0x02
+#define CRICKIT_MIN_PULSE 3277
+#define CRICKIT_MAX_PULSE 6554
+
 static struct bt_conn *default_conn;
 
-static const struct device *const i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c1));
+static const struct device *const i2c_dev = DEVICE_DT_GET(DT_NODELABEL(i2c0));
+
+uint32_t i2c_cfg = I2C_SPEED_SET(I2C_SPEED_STANDARD) | I2C_MODE_CONTROLLER;
 
 static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 			 struct net_buf_simple *ad)
@@ -31,17 +40,17 @@ static void device_found(const bt_addr_le_t *addr, int8_t rssi, uint8_t type,
 	bt_addr_le_to_str(addr, addr_str, sizeof(addr_str));
 
 	if (strncmp(addr_str, NODE_BASE, strlen(NODE_BASE)) == 0) {
-		// Majors: ad->data[25], ad->data[26]
-		// Minors: ad->data[27], ad->data[28]
-		printk("ad->data[25]: %u\n", ad->data[25]);
+		// // Majors: ad->data[25], ad->data[26]
+		// // Minors: ad->data[27], ad->data[28]
+		// printk("ad->data[25]: %u\n", ad->data[25]);
 	
-		printk("ad->data[26]: %u\n", ad->data[26]);
+		// printk("ad->data[26]: %u\n", ad->data[26]);
 
-		// Servo
-		printk("ad->data[27]: %u\n", ad->data[27]);
+		// // Servo
+		// printk("ad->data[27]: %u\n", ad->data[27]);
 
-		// true or false
-		printk("ad->data[28]: %u\n", ad->data[28]);
+		// // true or false
+		// printk("ad->data[28]: %u\n", ad->data[28]);
 	}
 }
 
@@ -51,40 +60,30 @@ int main(void)
 		return 0;
 	}
 
-	printk("The I2C scanner started\n");
-    // const struct device *i2c_dev;
-    int error;
+	int error;
 
-    // i2c_dev = device_get_binding("I2C_1");
-    if (!i2c_dev) {
-        printk("Binding failed.");
-        return 0;
-    }
+	if (!device_is_ready(i2c_dev)) {
+		printk("I2C device is not ready\n");
+		return 0;
+	}
 
-    /* Demonstration of runtime configuration */
-    i2c_configure(i2c_dev, I2C_SPEED_SET(I2C_SPEED_STANDARD));
-    printk("Value of NRF_TWIM2->PSEL.SCL : %d \n",NRF_TWIM1->PSEL.SCL);
-    printk("Value of NRF_TWIM2->PSEL.SDA : %d \n",NRF_TWIM1->PSEL.SDA);
-    printk("Value of NRF_TWIM2->FREQUENCY: %d \n",NRF_TWIM1->FREQUENCY);
-    printk("26738688 -> 100k\n");
+	/* 1. Verify i2c_configure() */
+	if (i2c_configure(i2c_dev, i2c_cfg)) {
+		printk("I2C config failed\n");
+		return 0;
+	}
 
-    // for (uint8_t i = 4; i <= 0x7F; i++) {
-    //     struct i2c_msg msgs[1];
-    //     uint8_t dst = 1;
+	// uint8_t msg[3] = {CRICKIT_SERVO, (uint8_t)(freq >> 8), (uint8_t)freq};
+	uint8_t msg[5] = {0x08, 0x01, CRICKIT_SERVO, 0xE0, 0xFF};
 
-    //     msgs[0].buf = &dst;
-    //     msgs[0].len = 1U;
-    //     msgs[0].flags = I2C_MSG_WRITE | I2C_MSG_STOP;
+	while(true) {
 
-    //     error = i2c_transfer(i2c_dev, &msgs[0], 1, i);
-    //     if (error == 0) {
-    //         printk("0x%2x FOUND\n", i);
-    //     }
-    //     else {
-    //         //printk("error %d \n", error);
-    //     }
+		error = i2c_write(i2c_dev, msg, 0, CRICKIT_ADDR);
+		error = i2c_write(i2c_dev, msg, 5, CRICKIT_ADDR);
+		printk("I2C error: %d\n", error);
+		k_msleep(1000);
 
-    // }
+	}
 
 	struct bt_le_scan_param scan_param = {
 		.type       = BT_HCI_LE_SCAN_PASSIVE,
